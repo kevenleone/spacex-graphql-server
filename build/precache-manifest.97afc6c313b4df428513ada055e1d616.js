1b4e3fa55e9129cd4841885894f09eb87666a82f
Keven Leone <keven.santos.sz@gmail.com>
2019-01-16 20:03:30 -0300

    Adicionado o build para execução da aplicaçao via node server

---
+++ build/precache-manifest.97afc6c313b4df428513ada055e1d616.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "831722bb0fb6111076a4",
+    "url": "/static/js/main.831722bb.chunk.js"
+  },
+  {
+    "revision": "da5a739afe9645d04508",
+    "url": "/static/js/1.da5a739a.chunk.js"
+  },
+  {
+    "revision": "a2d284e5626d13ab33e2128a1e735ff9",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const graphqlHttp = require('express-graphql')
 const app = express();
 const schema = require('./schema')
 const cors = require('cors');
+const path = require('path');
 
 app.use(cors());
 
@@ -10,6 +11,12 @@ app.use('/graphql', graphqlHttp({
     graphiql: true,
     schema
 }));
+
+app.use(express.static(path.join(__dirname, './build')));
+
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './build', 'index.html'))
+})
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log(`API Running on ${PORT}`));
